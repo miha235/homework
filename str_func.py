b6ed4de3817dc51ed42adce750d8dc6ea5287399
miha235 <68257131+miha235@users.noreply.github.com>
2024-05-04 15:47:30 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def to_uppercase2(input_string):
+    """
+    Преобразует строку в заглавные буквы.
+
+    Args:
+        s (str): Входная строка.
+
+    Returns:
+        str: Строка, преобразованная в заглавные буквы.
+    """
     return input_string.upper()
