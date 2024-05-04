318c7fd2ad045be0e799dcf782da378fcfd5e6d9
Victoria <Victoria23miha@gmail.com>
2024-05-04 16:02:14 +0300

    Добавлен docstring к функции

--- str_func.py
+++ str_func.py
@@ -1,2 +1,21 @@
 def to_uppercase2(input_string):
+    """
+        Преобразует строку в заглавные буквы.
+
+        Args:
+            s (str): Входная строка.
+
+        Returns:
+            str: Строка, преобразованная в заглавные буквы.
+    """
     return input_string.upper()
+
+
+def capitalize_words(sentence):
+    """
+    Capitalizes the first letter of each word in a sentence.
+
+    :param sentence: The input sentence.
+    :return: The modified sentence with the first letter of each word capitalized.
+    """
+    return ' '.join(word.capitalize() for word in sentence.split())
